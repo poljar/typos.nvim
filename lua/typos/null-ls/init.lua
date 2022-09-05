0061db8926a9d14dfa06099b2d63c3b6de21e6f3
Damir Jelić <poljar@termina.org.uk>
2022-09-05 19:54:35 +0200

    feat: Add support to act as a null-ls diagnostic source

---
+++ lua/typos/null-ls/init.lua
@@ -0,0 +1,40 @@
+local null_ls = require("null-ls")
+local utils = require('typos.utils')
+
+local M = {}
+
+local typos = {
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = {},
+    generator = null_ls.generator({
+        command = "typos",
+        args = {
+            "-",
+            "--format=json",
+        },
+        to_stdin = true,
+        ignore_stderr = true,
+        format = "json",
+        check_exit_code = function(code, stderr)
+            local success = code >= 0
+
+            if not success then
+                print(stderr)
+            end
+
+            return success
+        end,
+        on_output = function(params)
+            local diagnostics = {}
+
+            local diagnostic = utils.to_diagnostic(params.output)
+            table.insert(diagnostics, diagnostic)
+
+            return diagnostics
+        end
+    }),
+}
+
+M.diagnostic = typos
+
+return M
